aafb1e74fafd6876ca6c007079ce3f6d89f96e16
Willy Chang <ms70831@gmail.com>
2017-02-10 22:17:44 +0800

    create new TabLayoutEx tutorial

---
+++ app/src/main/java/com/devwilly/tutorial/tablayoutex/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devwilly.tutorial.tablayoutex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
