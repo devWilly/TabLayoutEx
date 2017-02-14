288a91970e965b0e4ded7f9d81058e3a12f40225
Willy Chang <ms70831@gmail.com>
2017-02-15 00:23:32 +0800

    create data wrapper

---
+++ app/src/main/java/com/devwilly/tutorial/tablayoutex/wrapper/ComingSoonMovieWrapper.java
@@ -0,0 +1,46 @@
+package com.devwilly.tutorial.tablayoutex.wrapper;
+
+import com.devwilly.tutorial.tablayoutex.R;
+
+import android.graphics.drawable.Drawable;
+
+
+/**
+ * Created by Willy on 2017/2/14.
+ */
+
+public class ComingSoonMovieWrapper implements IMovieWrapper {
+
+    private Drawable mDrawable;
+    private String mTitle;
+    private String mDate;
+    private String mSubTitle;
+
+    public ComingSoonMovieWrapper(Drawable d1, String tl1, String date1, String subTl1) {
+        this.mDrawable = d1;
+        this.mTitle = tl1;
+        this.mDate = date1;
+        this.mSubTitle = subTl1;
+    }
+
+    public Drawable getImage() {
+        return mDrawable;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDateRelease() {
+        return mDate;
+    }
+
+    public String getSubTitle() {
+        return mSubTitle;
+    }
+
+    @Override
+    public int getType() {
+        return R.layout.vh_item_coming_soon;
+    }
+}
---
+++ app/src/main/java/com/devwilly/tutorial/tablayoutex/wrapper/IMovieWrapper.java
@@ -0,0 +1,10 @@
+package com.devwilly.tutorial.tablayoutex.wrapper;
+
+/**
+ * Created by Willy on 2017/2/14.
+ */
+
+public interface IMovieWrapper {
+
+    int getType();
+}
