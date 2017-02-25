ace1b856f5849d764d9817b23bf2c74017d9c007
Willy Chang <ms70831@gmail.com>
2017-02-25 17:54:53 +0800

    create appWidget provider and it can resize

---
+++ app/src/main/java/com/devwilly/tutorial/tablayoutex/widget/MovieAppWidget.java
@@ -0,0 +1,12 @@
+package com.devwilly.tutorial.tablayoutex.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+
+/**
+ * Created by Willy on 2017/2/25.
+ */
+
+public class MovieAppWidget extends AppWidgetProvider {
+
+}
