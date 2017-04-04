a7a0eb2c0c130ad9da0cccd3c2b973aa560e82e9
Willy Chang <ms70831@gmail.com>
2017-04-04 20:06:44 +0800

    create custom ItemTouchHelper.Callback

---
+++ app/src/main/java/com/devwilly/tutorial/tablayoutex/helper/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,30 @@
+package com.devwilly.tutorial.tablayoutex.helper;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+
+/**
+ * Created by Willy on 2017/4/4.
+ */
+
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.UP;
+        int swipeFlags = ItemTouchHelper.START;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+            RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
+}
